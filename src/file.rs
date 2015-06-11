4f5c26d9e531f343fa6f7c34129d2b1258e6f22d
Evgeny Pashkin <epashkin@cognitive.ru>
2015-06-11 19:13:17 +0300

    Add GFile: second attempt

--- src/file.rs
+++ src/file.rs
@@ -4,7 +4,7 @@
 
 use ffi;
 use libc::{c_char};
-use object::{GenericObject, Ref/*, Upcast*/, Wrapper};
+use object::{GenericObject, Ref, Upcast, Wrapper};
 use translate::*;
 use types;
 
@@ -17,60 +17,53 @@ impl types::StaticType for File {
     }
 }
 
-impl File{
+pub trait FileExt{
     /// Constructs a File for a given path. This operation never fails,
     /// but the returned object might not support any I/O operation if path is malformed.  
-    pub fn new_for_path(path: &str)-> File {
+    fn new_for_path(path: &str)-> Self;
+    /// Constructs a GFile for a given URI. This operation never fails,
+    /// but the returned object might not support any I/O operation if uri is malformed or if the uri type is not supported.
+    fn new_for_uri(uri: &str)-> Self;
+    /// Gets the local pathname for GFile, if one exists.
+    /// This call does no blocking I/O.
+    fn get_path(&mut self) -> Option<String>;
+    /// Gets the URI for the file.
+    /// This call does no blocking I/O.
+    fn get_uri(&mut self) -> String;
+}
+
+impl<O: Upcast<File>> FileExt for O {
+    fn new_for_path(path: &str)-> Self {
         unsafe{
             let str2 = path.to_glib_none().0;
             let res: *mut ffi::GFile = ffi::g_file_new_for_path(str2);
             let res2: Ref = Ref::from_glib_full(res as *mut ffi::GObject);
-            let res3: File = File::wrap(res2);
+            let res3: Self = Self::wrap(res2);
             res3
         }
     }
-
-    pub fn new_for_uri(uri: &str)-> File {
+    fn new_for_uri(uri: &str)-> Self {
         unsafe{
             let str2 = uri.to_glib_none().0;
             let res: *mut ffi::GFile = ffi::g_file_new_for_uri(str2);
             let res2: Ref = Ref::from_glib_full(res as *mut ffi::GObject);
-            let res3: File = File::wrap(res2);
+            let res3: Self = Self::wrap(res2);
             res3
         }
     }
-    
-    pub fn get_path(&mut self) -> Option<String> {
+    fn get_path(&mut self) -> Option<String> {
         unsafe {
-            let this:*mut ffi::GFile = self.as_ref().to_glib_none() as *mut ffi::GFile;
+            let this = self.upcast().to_glib_none().0;
             from_glib_full(ffi::g_file_get_path(this) as *const c_char)
         }
     }
-
-    pub fn get_uri(&mut self) -> Option<String> {
+    fn get_uri(&mut self) -> String {
         unsafe {
-            let this:*mut ffi::GFile = self.as_ref().to_glib_none() as *mut ffi::GFile;
+            let this = self.upcast().to_glib_none().0;
             from_glib_full(ffi::g_file_get_uri(this) as *const c_char)
         }
     }
-  
 }
-
-/*
-pub trait FileExt {
-//  fn new_for_path(path: &str)-> FileExt;
-}
-
-impl<O: Upcast<File>> FileExt for O {
-/*  fn new_for_path(path: &str)-> FileExt {
-    unsafe{
-      let str2 = path.to_glib_none().0;
-      let res = ffi::g_file_new_for_path(str2);
-      from_glib(res);
-    }
-  }*/
-}
-*/ 
 
 #[cfg(test)]
 mod tests {
@@ -103,12 +96,12 @@ mod tests {
     #[test]
     fn get_uri_works_when_from_uri(){
       let mut file = File::new_for_uri("https://github.com/rust-gnome/glib");
-      file.get_uri().unwrap();
+      file.get_uri();
     }
     
     #[test]
     fn get_uri_works_when_from_path(){
       let mut file = File::new_for_path("a.txt");
-      file.get_uri().unwrap();
+      file.get_uri();
     }
 }
